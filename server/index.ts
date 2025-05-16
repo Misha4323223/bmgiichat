81995479a990019d95ee114f92e5655336828db0
Misha4323223 <Pimashin2015@gmail.com>
2025-05-16 17:08:41 +0000

    Initial commit from Replit

--- server/index.ts
+++ server/index.ts
@@ -1,21 +1,20 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
-import path from 'path';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Serve static files from the client build directory
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.get('/', (req: Request, res: Response) => {
+  res.send('<h1>Добро пожаловать на мой сайт!</h1>');
+});
 
-// Main route
-app.get('*', (_, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+app.get('*', (_: Request, res: Response) => {
+  res.status(404).send('Not Found');
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server started on http://localhost:${PORT}`);
 });
